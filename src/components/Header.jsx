3e4e2b656ddc72c6ae4cc53de6978ca0f6602c80
Sukrija Kararic <116309642+sukrijakararic@users.noreply.github.com>
2024-02-23 20:02:17 +0100

    added a header

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <div className='header'>
+        <h1>Good Eats!</h1>
+        <h3>Lets see what we can make..</h3>
+    </div>
+  )
+}

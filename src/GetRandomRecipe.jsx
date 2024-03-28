fb90352faf1f7e558991c4297f735c2161bf2a61
Sukrija Kararic <116309642+sukrijakararic@users.noreply.github.com>
2024-03-28 17:56:01 +0100

    implemented api succesfully

--- src/GetRandomRecipe.jsx
+++ src/GetRandomRecipe.jsx
@@ -1,22 +1,39 @@
-import React from 'react'
-import { useEffect } from 'react';
+import React, { useState } from "react";
 
-const apiKey = '2c286c913818405685a052e9d10d313a'
+const apiKey = "apiKey=2c286c913818405685a052e9d10d313a";
 
 export const GetRandomRecipe = () => {
+  const [popular, setPopular] = useState([]);
 
-    useEffect(() => {
-        getPopular();
-    },[]);
+  const getPopular = async () => {
+    const response = await fetch(
+      `https://api.spoonacular.com/recipes/random?${apiKey}&number=3&exclude-tags=pork,bacon`
+    );
+    const jsonResponse = await response.json();
+    console.log(jsonResponse);
+    setPopular(jsonResponse.recipes);
+  };
 
-    const getPopular = async () => {
-        const response = await fetch(`https://api.spoonacular.com/recipes/random?${apiKey}&number=10&exclude-tags=pork,bacon`);
-        const jsonResponse = await response.json();
-        console.log(jsonResponse);
-    };
+  const handleClick = () => {
+    getPopular();
+  };
 
   return (
     <div>
+      <button onClick={handleClick}>Generate</button>
+      {popular.map((recipe) => {
+        return (
+          <div key={recipe.id}>
+            <p>
+              {recipe.title} {recipe.dishTypes[0]}
+            </p>
+            <p>
+              <a>{recipe.sourceUrl}</a>
+            </p>
+            <img src={recipe.image} alt="image of the recipe" />
+          </div>
+        );
+      })}
     </div>
-  )
-}
+  );
+};

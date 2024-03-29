3f780be585ae8a29dcf35d481e77de68f3d3867b
Sukrija Kararic <116309642+sukrijakararic@users.noreply.github.com>
2024-03-29 17:56:01 +0100

    trialing a startup screen

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,14 @@
 import "../styles/App.css";
 import { Header } from "./Header";
 import { GetRandomRecipe } from "./GetRandomRecipe";
+import { Welcome } from "./welcome/Welcome";
 
 
 function App() {
 
   return (
     <div className="App">
+      <Welcome />
       <Header />
       <GetRandomRecipe />
     </div>

49b41da082bfd318ff579cf0dd0e05f629e7738f
Sukrija Kararic <116309642+sukrijakararic@users.noreply.github.com>
2024-03-30 12:34:18 +0100

    added loading screen functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,21 @@ import "../styles/App.css";
 import { Header } from "./Header";
 import { GetRandomRecipe } from "./GetRandomRecipe";
 import { Welcome } from "./welcome/Welcome";
+import { useEffect, useState } from "react";
 
 
 function App() {
 
+  const [welcome, setWelcome] = useState(true)
+    useEffect(() => {
+        setTimeout(() => setWelcome(false), 3300)
+    }, [])
+    if (welcome) {
+        return <Welcome />
+    }
+
   return (
     <div className="App">
-      <Welcome />
       <Header />
       <GetRandomRecipe />
     </div>

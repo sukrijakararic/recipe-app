34f13d390e2875120ccbbd3d10bbaa3b8ad234fd
Sukrija Kararic <116309642+sukrijakararic@users.noreply.github.com>
2024-03-31 15:13:38 +0200

    added another cuisine and made some style changes

--- src/components/ingredients/Cuisine.jsx
+++ src/components/ingredients/Cuisine.jsx
@@ -1,27 +1,45 @@
 import React, { useState } from "react";
 import styles from "./styles.module.css";
 
-export const Cuisine = ({setCuisine}) => {
-
+export const Cuisine = ({ setCuisine }) => {
   const handleClick = (e) => {
     setCuisine(e.target.id);
   };
 
   return (
     <div>
-      <h2>What cuisine would you like to eat?</h2>
-
       <form className={styles.cuisine}>
+        <h3>Pick a cuisine</h3>
+
         <div>
-          <input onClick={handleClick} type="radio" id="asian," name="cuisine" />
+          <input
+            onClick={handleClick}
+            type="radio"
+            id="asian,"
+            name="cuisine"
+          />
           <label htmlFor="asian,">Asian</label>
         </div>
 
         <div>
-          <input onClick={handleClick} type="radio" id="mediterranean," name="cuisine" />
+          <input
+            onClick={handleClick}
+            type="radio"
+            id="mediterranean,"
+            name="cuisine"
+          />
           <label htmlFor="mediterranean,">Mediterranean</label>
         </div>
 
+        <div>
+          <input
+            onClick={handleClick}
+            type="radio"
+            id="italian,"
+            name="cuisine"
+          />
+          <label htmlFor="italian,">Italian</label>
+        </div>
       </form>
     </div>
   );

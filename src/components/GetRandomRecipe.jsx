6dfa48d84b8fe5625c06fbb36d384d14566cc286
Sukrija Kararic <116309642+sukrijakararic@users.noreply.github.com>
2024-04-01 15:59:46 +0200

    added try/catch statement

--- src/components/GetRandomRecipe.jsx
+++ src/components/GetRandomRecipe.jsx
@@ -1,24 +1,23 @@
-import React  from "react";
+import React from "react";
 
 const apiKey = "apiKey=2c286c913818405685a052e9d10d313a";
 
-export const GetRandomRecipe = ({protein, cuisine, setPopular, popular}) => {
-
-  console.log(protein) // dont forget to delete these before production 
-  console.log(cuisine)
+export const GetRandomRecipe = ({ protein, cuisine, setPopular, popular }) => {
 
 
-  
   const getPopular = async () => {
-
-    const response = await fetch(
-      `https://api.spoonacular.com/recipes/complexSearch?${apiKey}&number=10&query=${cuisine}${protein}&maxAlcohol=0&`
-    );
-    const jsonResponse = await response.json();
-    console.log(jsonResponse);
-    const resultsFromArray = jsonResponse.results
-    console.log(resultsFromArray);
-    setPopular(resultsFromArray)
+    try {
+      const response = await fetch(
+        `https://api.spoonacular.com/recipes/complexSearch?${apiKey}&number=10&query=${cuisine}${protein}&maxAlcohol=0&`
+      );
+      const jsonResponse = await response.json();
+      console.log(jsonResponse);
+      const resultsFromArray = jsonResponse.results;
+      console.log(resultsFromArray);
+      setPopular(resultsFromArray);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   const handleClick = () => {
@@ -27,13 +26,11 @@ export const GetRandomRecipe = ({protein, cuisine, setPopular, popular}) => {
 
   return (
     <div>
-    <button onClick={handleClick}>Generate</button>
+      <button onClick={handleClick}>Generate</button>
       {popular.map((recipe) => {
         return (
           <div key={recipe.id}>
-            <p>
-              {recipe.title}
-            </p>
+            <p>{recipe.title}</p>
             <img src={recipe.image} alt="a picture of the food" />
           </div>
         );

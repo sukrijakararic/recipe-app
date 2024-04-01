3f0ee4e93b2a34083c906c3f363809db1768cbb5
Sukrija Kararic <116309642+sukrijakararic@users.noreply.github.com>
2024-04-01 13:51:38 +0200

    changed api call

--- src/components/GetRandomRecipe.jsx
+++ src/components/GetRandomRecipe.jsx
@@ -11,12 +11,15 @@ export const GetRandomRecipe = ({protein, cuisine}) => {
 
   
   const getPopular = async () => {
+
     const response = await fetch(
-      `https://api.spoonacular.com/recipes/random?${apiKey}&number=3&include-tags=${cuisine}${protein}`
+      `https://api.spoonacular.com/recipes/complexSearch?${apiKey}&number=30&query=${cuisine}${protein}&maxAlcohol=0&offset=0`
     );
     const jsonResponse = await response.json();
     console.log(jsonResponse);
-    setPopular(jsonResponse.recipes);
+    const resultsFromArray = jsonResponse.results
+    console.log(resultsFromArray);
+    setPopular(resultsFromArray)
   };
 
   const handleClick = () => {
@@ -25,15 +28,15 @@ export const GetRandomRecipe = ({protein, cuisine}) => {
 
   return (
     <div>
-      <button onClick={handleClick}>Generate</button>
+    <button onClick={handleClick}>Generate</button>
       {popular.map((recipe) => {
         return (
           <div key={recipe.id}>
             <p>
-              {recipe.title} {recipe.dishTypes[0]}
+              {recipe.title}
             </p>
-            <p>{recipe.sourceUrl}</p>
-            <img src={recipe.image} alt="image of the recipe" />
+            <p></p>
+            <img src={recipe.image} alt="a picture of the food" />
           </div>
         );
       })}

c839167623aee3d11a4fe1a53827a4ffda6a1742
Sukrija Kararic <116309642+sukrijakararic@users.noreply.github.com>
2024-04-12 16:22:28 +0200

    added boiler plate to another api component

--- src/components/apiSettings/GetRecipeByIngredients.jsx
+++ src/components/apiSettings/GetRecipeByIngredients.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import Aos from "aos";
+import "aos/dist/aos.css";
+import apiStyling from "./apiStyling.module.css";
+import "animate.css";
+
+
+const apiKey = "apiKey=2c286c913818405685a052e9d10d313a";
 
 export const GetRecipeByIngredients = () => {
+
+  useEffect(() => {
+    Aos.init();
+  }, []);
+
+  const [ingredientslist, setIngredientslist] = useState([]);
+
+  try {
+    
+  } catch (error) {
+    console.log(error);
+  }
+
   return (
-    <div>Work in Progress</div>
+    <div>
+
+
+    </div>
   )
 }

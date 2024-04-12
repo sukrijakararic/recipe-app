8ea91341584f2d2d61b6157c244885526e906f7d
Sukrija Kararic <116309642+sukrijakararic@users.noreply.github.com>
2024-04-12 18:08:49 +0200

    added the api key and refactored the fetch request for the ingredients api component.

--- src/components/apiSettings/GetRecipeByIngredients.jsx
+++ src/components/apiSettings/GetRecipeByIngredients.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 
+const apiKey = "apiKey=2c286c913818405685a052e9d10d313a";
 export const GetRecipeByIngredients = () => {
   const callRecipeApiForIngredients = async () => {
     try {
       const response = await fetch(
-        "https://jsonplaceholder.typicode.com/todos/1"
+        `https://api.spoonacular.com/recipes/findByIngredients?${apiKey}&ingredients=apples&number=5`
       );
       const jsonResponse = await response.json();
       console.log(jsonResponse);

44ec28ba71e06ae56f27fcc4a9d3d8a1d395b079
Sukrija Kararic <116309642+sukrijakararic@users.noreply.github.com>
2024-04-12 19:33:08 +0200

    changed the api endpoint for more info from json

--- src/components/apiSettings/GetRecipeByIngredients.jsx
+++ src/components/apiSettings/GetRecipeByIngredients.jsx
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-const apiKey = "apiKey=2c286c913818405685a052e9d10d313a";
+const apiKey = "apiKey=4bf6c458c2cf48b4ab3bc26b7eda5a57";
 export const GetRecipeByIngredients = () => {
+  const [ingredientsRecipe, setIngredientsRecipe] = useState([]);
   const callRecipeApiForIngredients = async () => {
     try {
       const response = await fetch(
-        `https://api.spoonacular.com/recipes/findByIngredients?${apiKey}&ingredients=apples&number=5`
+        `https://api.spoonacular.com/recipes/complexSearch?${apiKey}&includeIngredients=apples,flour,sugar,milk&number=5&&addRecipeInformation=true`
       );
       const jsonResponse = await response.json();
       console.log(jsonResponse);
+      console.log(jsonResponse.results);
+      setIngredientsRecipe(jsonResponse.results);
     } catch (error) {
       console.log(error.message);
     }
@@ -21,6 +24,16 @@ export const GetRecipeByIngredients = () => {
   return (
     <div>
       <button onClick={handleClick}>Get Recipe By Ingredients</button>
+      {ingredientsRecipe.map((recipe) => (
+        <div key={recipe.id}>
+          <h3>{recipe.title}</h3>
+          <img src={recipe.image} alt={recipe.title} />
+          <br></br>
+          <a href={recipe.sourceUrl} target="_blank">
+            Click Me
+          </a>
+        </div>
+      ))}
     </div>
   );
 };

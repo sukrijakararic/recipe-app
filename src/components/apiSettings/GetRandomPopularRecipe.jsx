cd847e73e4b279c2520f09e01eda8f97d1529797
Sukrija Kararic <116309642+sukrijakararic@users.noreply.github.com>
2024-05-15 19:49:32 +0200

    created function to search for popular recipes using api

--- src/components/apiSettings/GetRandomPopularRecipe.jsx
+++ src/components/apiSettings/GetRandomPopularRecipe.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, { useState } from "react";
+import apiStyling from "./apiStyling.module.css";
 
+const apiKey = "91e5d21482464f81a6df62753eb70fe6";
 export const GetRandomPopularRecipe = () => {
+  const handleClick = () => {
+    getPopularRecipe();
+  };
+
+  const [random, setRandom] = useState([]);
+  const getPopularRecipe = async () => {
+    try {
+      const response = await fetch(
+        `https://api.spoonacular.com/recipes/random/?apiKey=${apiKey}&number=3&exclude-tags=pork`
+      );
+      const jsonResponse = await response.json();
+      setRandom(jsonResponse.recipes);
+      console.log(jsonResponse);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
   return (
-    <div>Work in Progress</div>
-  )
-}
+    <div>
+      <button onClick={handleClick}>
+        <span className={apiStyling.lets}>Lets</span>{" "}
+        <span className={apiStyling.eat}>Eat!</span>
+      </button>
+    </div>
+  );
+};
